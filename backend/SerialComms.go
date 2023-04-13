fbd534984792b0df867379b2599d1e3c44e1685a
Aeyu17 <jami3507@gmail.com>
2023-04-13 14:03:44 -0500

    fixing serial comms cause john is dumb

--- backend/SerialComms.go
+++ backend/SerialComms.go
@@ -1,43 +1,45 @@
-package backend;
+package backend
 
-// import (
-// 	"log"
-// 	"serial-master"
-// 	"strings"
-// )
+import (
+	"log"
+	"strings"
 
-// func GetPacket(port string, baud int) (data string) {
-// 	c := &serial.Config{Name: port, Baud: baud}
-// 	s, err := serial.OpenPort(c)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	buf := make([]byte, 128)
-// 	for {
-// 		n, err := s.Read(buf)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		// If we receive a newline stop reading
-// 		if strings.Contains(string(buf[:n]), "\n") {
-// 			data = string(buf[:n])
-// 			break
-// 		}
-// 	}
-// 	s.Close()
-// 	return
-// }
+	"github.com/tarm/serial"
+)
 
-// func SendPacket(port string, baud int,  data string) {
-// 	c := &serial.Config{Name: port, Baud: baud}
-// 	s, err := serial.OpenPort(c)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	n, err := s.Write([]byte(data))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		if (n == 1) {} // unused variable I am currently unsure how to handle
-// 	}
-// 	s.Close()
-// }
+func GetPacket(port string, baud int) (data string) {
+	c := &serial.Config{Name: port, Baud: baud}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 128)
+	for {
+		n, err := s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// If we receive a newline stop reading
+		if strings.Contains(string(buf[:n]), "\n") {
+			data = string(buf[:n])
+			break
+		}
+	}
+	s.Close()
+	return
+}
+
+func SendPacket(port string, baud int, data string) {
+	c := &serial.Config{Name: port, Baud: baud}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	n, err := s.Write([]byte(data))
+	if err != nil {
+		log.Fatal(err)
+		if (n == 1) {} // unused variable I am currently unsure how to handle
+	}
+	s.Close()
+}
