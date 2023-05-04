42361dceba9e61abe0b70c1764e6451a108945f0
Aeyu17 <jami3507@gmail.com>
2023-05-04 14:49:26 -0500

    CAMERA WORKS WOOOO

--- embedded/camera_isolated/camera_isolated.ino
+++ embedded/camera_isolated/camera_isolated.ino
@@ -1,41 +1,33 @@
-const int trig = 33;
-const int led = LED_BUILTIN;
-
+const int trig = 26;
 
 void setup() {     
   Serial.begin(115200); 
 
-  delay(500);
-  
-  if (Serial)
-  {
-    Serial.println("WORKING");
-  }
-  
-  // initialize the digital pins as output.
-  pinMode(led, OUTPUT);
   pinMode(trig, OUTPUT);         
 
-  digitalWrite(led, HIGH);
-  digitalWrite(trig, LOW); 
-
-  Serial.println("MADE IT HERE");
-
-  delay(1000);
-
-  // start recording
-  Serial.println("Recording...");
-  digitalWrite(trig, HIGH);
-  delay(10);
-  digitalWrite(trig, LOW);
-  
+  startRecording();
   delay(10000);
+  stopRecording();
 
-  Serial.println("HIGH");
+
+}
+
+void startRecording() {
+  Serial.println("Recording...");
+  digitalWrite(trig, LOW); 
+  delay(1000);
+  digitalWrite(trig, HIGH);
+  Serial.println("Recording started.");
+}
+
+void stopRecording() {
+  Serial.println("Stopping the recording...");
+  digitalWrite(trig, LOW);
+  delay(1000);
   digitalWrite(trig, HIGH);
   delay(10);
-  Serial.println("LOW");
   digitalWrite(trig, LOW);
+  Serial.println("Recording stopped.");
 }
 
 void loop(){;}
